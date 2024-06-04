184242332a2fb653d124a00000f71a356bed1c00
Noelia Rico <noeliarico@uniovi.es>
2024-06-04 17:15:13 +0200

    🥇 Primera versión en pypi

---
+++ build/lib/pybopa/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for pybopa."""
+
+__author__ = """Noelia Rico"""
+__email__ = 'noeliarico@uniovi.es'
+__version__ = '0.1.0'
+
+from .pybopa import *
---
+++ build/lib/pybopa/pybopa.py
@@ -0,0 +1,4 @@
+"""Main module."""
+
+def saluda():
+    print("Hola mundo")
